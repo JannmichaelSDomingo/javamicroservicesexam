3a527def192db2aca749073fe3035d72a7621f59
Jannmichael Domingo <jsdomingo@exist.com>
2024-06-13 15:19:26 +0800

    improve code

--- src/main/java/com/example/javamicroservice/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/javamicroservice/service/impl/AccountServiceImpl.java
@@ -15,17 +15,35 @@ import java.util.Optional;
 @Transactional
 public class AccountServiceImpl implements AccountService {
 
+    private final AccountRepo accountRepo;
+
     @Autowired
-    private AccountRepo accountRepo;
+    public AccountServiceImpl(AccountRepo accountRepo) {
+        this.accountRepo = accountRepo;
+    }
 
     @Override
     public Account create(Account account) {
-        return accountRepo.save(account);
+        log.info("Creating new account for customer number: {}", account.getCustomerNumber());
+        try {
+            Account createdAccount = accountRepo.save(account);
+            log.info("Account created successfully for customer number: {}", createdAccount.getCustomerNumber());
+            return createdAccount;
+        } catch (Exception e) {
+            log.error("Failed to create account for customer number: {}", account.getCustomerNumber(), e);
+            throw new RuntimeException("Account creation failed", e);
+        }
     }
 
     @Override
     public Optional<Account> get(Long customerNumber) {
-        return accountRepo.findById(customerNumber);
+        log.info("Retrieving account for customer number: {}", customerNumber);
+        Optional<Account> account = accountRepo.findById(customerNumber);
+        if (account.isPresent()) {
+            log.info("Account retrieved successfully for customer number: {}", customerNumber);
+        } else {
+            log.warn("No account found for customer number: {}", customerNumber);
+        }
+        return account;
     }
-
 }
